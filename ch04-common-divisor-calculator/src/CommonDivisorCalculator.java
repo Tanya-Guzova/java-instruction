1a69c9115a87023fa73b0852caf03cdaeb78bb77
Tanya Guzova <t.guzoff@gmail.com>
2020-09-29 10:12:06 -0400

    CommonDivisorCalculator App

--- ch04-common-divisor-calculator/src/CommonDivisorCalculator.java
+++ ch04-common-divisor-calculator/src/CommonDivisorCalculator.java
@@ -6,47 +6,37 @@ public class CommonDivisorCalculator {
 		System.out.println("Greatest Common Divisor Finder");
 		String choice = "y";
 		Scanner sc = new Scanner(System.in);
-		
+
+		// *while loop starts
 		while (choice.equalsIgnoreCase("y")) {
+			System.out.println();
+			// get input from user -x, y as ints
 			System.out.println("Enter first number: ");
-			int firstNumber = sc.nextInt();
+			int x = sc.nextInt();
 			System.out.println("Enter second number: ");
-			int secondNumber = sc.nextInt();
-			int gcd = 0;
-			
-			 if (firstNumber > secondNumber) {
-				 while (firstNumber > secondNumber) {
-					 firstNumber = firstNumber - secondNumber;
-					 gcd = firstNumber;
-					 
-					 while (secondNumber > firstNumber) {
-						 secondNumber = secondNumber - firstNumber;
-						 gcd = secondNumber;
-					 }
-				 }
-				
-			 }
-			 if (secondNumber > firstNumber) {
-				 while (secondNumber > firstNumber) {
-					 secondNumber = secondNumber - firstNumber;
-					 gcd = secondNumber;
-					 
-//					 while (firstNumber > secondNumber) {
-//						 firstNumber = firstNumber - secondNumber;
-//						 gcd = firstNumber;
-//					 }
-				 }
-				
-			 }
-			 			
-			 System.out.println("Greatest Common Divisor: " + gcd );
+			int y = sc.nextInt();
+			System.out.println();
+			while (x > 0) {
+				while (y>= x) {
+					y -= x;
+				}
+				// swap x and y
+				int tempY = y;
+				y = x;
+				x = tempY;
+			}
+
+			// display output - gcd as int
+			// y is gcd
+			System.out.println("Greatest Common Divisor: " + y);
+			System.out.println();
 			System.out.println("Continue? (y/n): ");
 			choice = sc.next();
-			
+			// *while loop ends
 		}
 		sc.close();
 		System.out.println("Bye!");
-	
+
 	}
 
 }
